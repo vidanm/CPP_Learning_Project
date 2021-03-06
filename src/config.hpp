5cea6a2b48390def4eb36b825a1a8cf30f5c559f
Laefy <laefy22@gmail.com>
2021-03-06 18:46:47 +0100

    C-3.

--- src/config.hpp
+++ src/config.hpp
@@ -8,7 +8,8 @@ const MediaPath one_lane_airport_sprite_path = { "airport_1lane.png" };
 const MediaPath two_lane_airport_sprite_path = { "airport_2lane.png" };
 
 // number of cycles needed to service an aircraft at a terminal
-constexpr unsigned int SERVICE_CYCLES = 20u;
+// TASK-0 C-3
+constexpr unsigned int SERVICE_CYCLES = 40u;
 // speeds below the threshold speed loose altitude linearly
 constexpr float SPEED_THRESHOLD = 0.05f;
 // this models the speed with wich slow (speed < SPEED_THRESHOLD) aircrafts sink
