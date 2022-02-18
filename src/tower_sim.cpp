ed08c83782c5bf350f3a53f12aa2b44add194909
vidanm <vidan.tauriff@gmail.com>
2022-02-18 17:51:49 +0000

    ticks + concorde speed

--- src/tower_sim.cpp
+++ src/tower_sim.cpp
@@ -57,6 +57,8 @@ void TowerSimulation::create_keystrokes() const
     GL::keystrokes.emplace('+', []() { GL::change_zoom(0.95f); });
     GL::keystrokes.emplace('-', []() { GL::change_zoom(1.05f); });
     GL::keystrokes.emplace('f', []() { GL::toggle_fullscreen(); });
+		GL::keystrokes.emplace('p', []() { GL::ticks_per_sec++;});
+		GL::keystrokes.emplace('o', []() { GL::ticks_per_sec--;});
 }
 
 void TowerSimulation::display_help() const
